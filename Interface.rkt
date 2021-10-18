ba2f388806094f88623931d1fcb60dbd49b278e1
Carlos Akión Garro Campos <carloscamp1008@estudianTEC.cr>
2021-10-17 21:06:19 -0600

    Interface Creation

--- Interface.rkt
+++ Interface.rkt
@@ -1,22 +1,58 @@
 
 #lang racket
-(require racket/gui/base)
+(require graphics/graphics)
+(require 2htdp/image)
+(require racket/math)
 
-; Definir la imagen de fondo
-(define the-bitmap
-  (make-object bitmap% "background.jpg"))
 
-(define frame (new frame%
-                   [label "CatarCEQualifiers"]
-                   [width 975]
-                   [height 640]))
-(new canvas% [parent frame]
-             [paint-callback
-              (lambda (canvas dc)
-                (send dc set-scale 1 1)
-                (send dc draw-bitmap the-bitmap 0 0)
-                (send dc set-text-foreground "blue")
-                (send dc draw-text "●" 0 0))
-              ])
+;Se inicializa la libreria encargada del uso de la parte grafica.
+(open-graphics)
 
-(send frame show #t)
+
+;Se crea la ventana y el mapa de pixeles con el mismo tamaño de la ventana
+(define ventana (open-viewport "CatarCEQualifiers" 1220 530))
+(define mapaDePixeles (open-pixmap "CatarCEQualifiers" 1220 530))
+
+
+#|
+
+--------------------------------------Definicion para dibujar la cancha----------------------------------------
+
+Se dibuja cada parte de la cancha utilizando rectangulos y elipses
+|#
+(define (dibujarCancha)
+  (begin
+    ((draw-solid-rectangle mapaDePixeles) (make-posn 0 0) 1220 720 "black") ;;Fondo
+    ((draw-solid-rectangle mapaDePixeles) (make-posn 0 0) 920 540 "Cyan") ;;Cesped
+    ((draw-solid-rectangle mapaDePixeles) (make-posn 0 0) 5 540 "black")   ;;Raya vertical
+    ((draw-solid-rectangle mapaDePixeles) (make-posn 915 0) 5 540 "black") ;;Raya vertical
+    ((draw-solid-rectangle mapaDePixeles) (make-posn 0 0) 920 5 "black")   ;;Raya horizontal
+    ((draw-solid-rectangle mapaDePixeles) (make-posn 0 535) 920 5 "black") ;;Raya horizontal
+    ((draw-solid-rectangle mapaDePixeles) (make-posn 457 0) 5 540 "black") ;;Raya medio campo
+    ((draw-solid-ellipse mapaDePixeles) (make-posn 452 262) 15 15 "black") ;;Circulo central
+    ((flip-ellipse mapaDePixeles) (make-posn 385 195) 150 150 "black")     ;;Circulo grande central
+    ((flip-rectangle mapaDePixeles) (make-posn -90 180) 180 180 "black")   ;;Marco izquierdo
+    ((flip-rectangle mapaDePixeles) (make-posn -90 100) 350 350 "black")   ;;Area grande
+    ((flip-rectangle mapaDePixeles) (make-posn 830 180) 90 180 "black")   ;;Marco derecho
+    ((flip-rectangle mapaDePixeles) (make-posn 650 100) 350 350 "black")   ;;Area grande
+    ((draw-pixmap mapaDePixeles) "background.jpg" (make-posn 0 0))
+   
+   
+    ))
+
+;;Borra la pantalla 2 y pega la pantalla 2 en la principal.
+;;E: No tiene.
+;;S: Borra la pantalla y la pega en la principal.
+(define (drawWindow)
+  (begin
+    (copy-viewport mapaDePixeles ventana)
+    ((clear-viewport mapaDePixeles))
+    )
+  )
+
+
+
+(dibujarCancha)
+(drawWindow)
+
+
