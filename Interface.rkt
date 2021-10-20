e4ddd7c2d8a6ffc5e080ec1a85e0e5373ecd679c
johnnyzaet08 <johnny.zaet08@gmail.com>
2021-10-19 23:46:48 -0600

    Creacion de la primer generacion

--- Interface.rkt
+++ Interface.rkt
@@ -9,6 +9,83 @@
 
 
 
+
+#|
+------------------------------------Primera generacion para ambos teams------------------------------------
+Def: Estas funciones se encargan de crear cada player con sus posicion, equipo y skills.
+     Tambien se encargan de crear el equipo segun la formacion requerida por el usuario.
+     Se generan por separado cada primera generacion de cada equipo, las skills son random.
+|#
+
+;-----------------------Crea skills, posicion y skills random------------------------
+
+(define (player-1er tipo num)
+  (cond((equal? 1 tipo)
+        (list (list 5 (+ 180 (random 180))) (list 0 0) (list 0 (random 11) (random 11) (random 11) (random 11)) 1 num "red"))
+       ((equal? 2 tipo)
+        (list (list (+ 90 (random 50)) (+ 5 (random 510))) (list 0 0) (list 0 (random 11) (random 11) (random 11) (random 11)) 2 num "red"))
+       ((equal? 3 tipo)
+        (list (list (+ 260 (random 50)) (+ 5 (random 510))) (list 0 0) (list 0 (random 11) (random 11) (random 11) (random 11)) 3 num "red"))
+       (else
+        (list (list (+ 400 (random 50)) (+ 5 (random 510))) (list 0 0) (list 0 (random 11) (random 11) (random 11) (random 11)) 4 num "red"))
+       ))
+
+(define (player-2do tipo num)
+  (cond ((equal? 1 tipo)
+         (list (list 895 (+ 180 (random 180))) (list 0 0) (list 0 (random 11) (random 11) (random 11) (random 11)) 1 num "blue"))
+        ((equal? 2 tipo)
+         (list (list (+ 750 (random 50)) (+ 5 (random 510))) (list 0 0) (list 0 (random 11) (random 11) (random 11) (random 11)) 2 num "blue"))
+        ((equal? 3 tipo)
+         (list (list (+ 600 (random 50)) (+ 5 (random 510))) (list 0 0) (list 0 (random 11) (random 11) (random 11) (random 11)) 3 num "blue"))
+        (else
+         (list (list (+ 465 (random 50)) (+ 5 (random 510))) (list 0 0) (list 0 (random 11) (random 11) (random 11) (random 11)) 4 num "blue"))
+        ))
+
+;--------------------------Verifica formacion y le pone un numero al player---------------------
+
+(define (inic-aux1 num formacion)
+  (cond((zero? num) '())
+       ((equal? 1 num)
+        (cons (player-1er 1 1) (inic-aux1 (- num 1) formacion)))
+       ((and (> num 1) (< num (+ 2 (car formacion))))
+        (cons (player-1er 2 num) (inic-aux1 (- num 1) formacion)))
+       ((and (> num (+ 1 (car formacion))) (< num (+ 2 (car formacion) (cadr formacion))))
+        (cons (player-1er 3 num) (inic-aux1 (- num 1) formacion)))
+       (else
+        (cons (player-1er 4 num) (inic-aux1 (- num 1) formacion)))
+       ))
+
+(define (inic-aux2 num formacion)
+  (cond ((zero? num) '())
+        ((equal? 1 num)
+         (cons (player-2do 1 1) (inic-aux2 (- num 1) formacion)))
+        ((and (> num 1) (< num (+ 2 (car formacion))))
+         (cons (player-2do 2 num) (inic-aux2 (- num 1) formacion)))
+        ((and (> num (+ 1 (car formacion))) (< num (+ 2 (car formacion) (cadr formacion))))
+         (cons (player-2do 3 num) (inic-aux2 (- num 1) formacion)))
+        (else
+         (cons (player-2do 4 num) (inic-aux2 (- num 1) formacion)))
+        ))
+
+;--------------------------Obtiene la formacion para ir creando playeres---------------------------
+
+(define (inicialitation1 formacion)
+  (cond ((not(list? formacion)) '())
+        (else
+         (inic-aux1 (+ 1 (car formacion) (cadr formacion) (caddr formacion)) formacion))
+        ))
+
+(define (inicialitation2 formacion)
+  (cond ((not(list? formacion)) '())
+        (else
+         (inic-aux2 (+ 1 (car formacion) (cadr formacion) (caddr formacion)) formacion))
+        ))
+
+
+
+
+
+
 #|
 --------------------------------------Creacion de las ventanas----------------------------------------
 Def: Se definen las funciones para crear una ventana de tipo viewport y una ventana de tipo pixmap, con
